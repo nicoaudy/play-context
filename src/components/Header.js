5444957b5ca30f50581e032474b05698ed82c4d8
Nico Audy <nicoaudy1994@gmail.com>
2020-02-24 09:29:10 +0700

    design components

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <>Hi im header</>;
+};
+
+export default Header;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header";
+import Balance from "./Balance";
+import Income from "./Income";
+import TransactionList from "./TransactionList";
+import AddTransaction from "./AddTransaction";
+
+export { Header, Balance, Income, TransactionList, AddTransaction };
