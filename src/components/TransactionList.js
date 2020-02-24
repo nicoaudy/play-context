7d584adb063a216378dd7f0044b114aec86c9adf
Nico Audy <nicoaudy1994@gmail.com>
2020-02-24 10:39:03 +0700

    so easy using context

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const TransactionList = () => {
+  const context = useContext(GlobalContext);
+
   return (
     <div>
       <h3>History</h3>
-      <ul id="list" className="list">
-        {/* <li class="minus">
-          Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li> */}
-      </ul>
+      {context.transactions && (
+        <ul id="list" className="list">
+          {context.transactions.map((row, i) => {
+            return (
+              <li className="minus" key={i}>
+                {row.text} <span>{row.amount}</span>
+                <button className="delete-btn">x</button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
