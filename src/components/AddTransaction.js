2a4211642ec42b47086fd1b9230ebcf49ec4d09e
Nico Audy <nicoaudy1994@gmail.com>
2020-02-24 09:35:45 +0700

    set the state to transactions

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
   return (
     <div>
       <h3>Add new transaction</h3>
       <form id="form">
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input
+            type="text"
+            id="text"
+            placeholder="Enter text..."
+            value={text}
+            onChange={e => setText(e.target.value)}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input
+            type="number"
+            id="amount"
+            placeholder="Enter amount..."
+            value={amount}
+            onChange={e => setAmount(e.target.value)}
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
